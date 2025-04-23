a8fd867751c18bd05ed2880633063dc83b4dc41b
Thiago Moreira Theodoro <thiagomoreiraf273@gmail.com>
2025-04-23 18:00:35 -0300

    primeira versão funcional do chat lan

--- public/app.js
+++ public/app.js
@@ -1,2 +1,65 @@
 const socket = io();
+const nome = document.getElementById('name-user');
+const color = document.getElementById('color');
+const chatlog = document.getElementById('chat-log');
 
+let currentColor = 'Black'
+
+function changeName(){
+    let newName = ''
+    while (newName === '' || newName === null){
+        newName = window.prompt('Digite o seu nome: ');
+    }
+    console.log(newName);
+    nome.textContent = newName;
+    socket.emit('change_name', [newName, currentColor]);
+}
+
+function createChatBubble(name, color, text){
+    const newChat = document.createElement('div');
+    newChat.classList.add('chat');
+
+    if (name && color) {
+        const nameDiv = document.createElement('div');
+        nameDiv.classList.add('name-user-chat');
+        nameDiv.innerText = name;
+        nameDiv.style.color = color;
+        newChat.appendChild(nameDiv);
+    } else {
+        newChat.classList.add('chat-self');
+    }
+
+    const textDiv = document.createElement('div');
+    textDiv.classList.add('chat-text');
+    textDiv.innerText = text;
+    newChat.appendChild(textDiv);
+
+    return newChat;
+}
+
+function sendChat(key) {
+    if(key){
+        if(event.keyCode !== 13) return
+    }
+    let text = document.getElementById('input-text').value;
+    if(!text) return;
+    socket.emit('send', text);
+    document.getElementById('input-text').value = '';
+}
+
+socket.on('first_entry', (usuario) => {
+    if(usuario.nome === '') changeName();
+    else {
+        nome.innerText = usuario.nome;
+        currentColor = usuario.cor;
+    }
+})
+
+socket.on('update', (chatlist, ipAtual) => {
+    chatlog.innerHTML = '';
+    chatlist.forEach(chat => {
+        if(chat.ip === ipAtual) chatlog.appendChild(createChatBubble('', '', chat.text))
+        else chatlog.appendChild(createChatBubble(chat.name, chat.color, chat.text));
+        console.log(chat);
+    });
+})
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+
 //Pasta que sera enviada na rede
 app.use(express.static('public'));
 
@@ -14,26 +15,52 @@ app.use(express.static('public'));
 let userList = [];
 let chatLog = [];
 
-io.on('connection', (socket) => {
-  console.log(`usuario de id "${socket.id}" conectado`);
+//Função que confere o ip do usuario ou adiciona um usuario novo
+function addOrUpdateUser(ip, id) {
+  const user = userList.find(u => u.ip === ip);
+  if (user) {
+    user.id = id;
+  } else {
+    userList.push({ id, ip, nome: '', cor: ''});
+  }
+}
 
-  userList.push({
-    id: socket.id,
-    ip: '',
-    nome: ''
-  });
+
+io.on('connection', (socket) => {
+  const ipAdress = socket.handshake.address;
+  console.log(`usuario de id "${socket.id}" conectou pelo ip ${ipAdress}`);
+
+  addOrUpdateUser(ipAdress, socket.id);
+  const currentUser = userList.find(u => u.ip === ipAdress);
+
+  socket.emit('first_entry', currentUser);
+
+  socket.on('change_name', (data) => {
+    currentUser.nome = data[0] === null? currentUser.nome: data[0];
+    currentUser.cor = data[1];
+    console.log(`Tem ${userList.length} usuários conectados \n`, userList);
+  })
 
   //Envio de mensagem
   socket.on('send', (text) => {
-    chatLog.push({
-      
+    chatLog.push(
+      {
+        name: currentUser.nome,
+        color: currentUser.cor,
+        ip: currentUser.ip,
+        text: text
+      }
+    )
+    userList.forEach(user => {
+      io.to(user.id).emit('update', chatLog, currentUser.ip);
     })
   })
 
+  console.log(userList);
 });
 
 
 
-app.listen(3000, () => {
-  console.log(`Server online in http://${ip.address()}:3000`);
+server.listen(3000, () => {
+  console.log(`Server online at http://${ip.address()}:3000`);
 });
