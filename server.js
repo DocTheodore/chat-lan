9dff7051566072bd6181d7de65ad420809527fc4
Thiago Moreira Theodoro <thiagomoreiraf273@gmail.com>
2025-04-23 09:58:46 -0300

    estrutura inicial do servidor

--- server.js
+++ server.js
@@ -11,6 +11,27 @@ const port = 3000;
 //Pasta que sera enviada na rede
 app.use(express.static('public'));
 
+//Lista de Usuarios
+let userList = [];
+let chatLog = [];
+
+io.on('connection', (socket) => {
+  console.log(`usuario de id "${socket.id}" conectado`);
+
+  userList.push({
+    id: socket.id,
+    ip: '',
+    nome: ''
+  });
+
+  //Envio de mensagem
+  socket.on('send', (text) => {
+    chatLog.push({
+      
+    })
+  })
+
+});
 
 
 
